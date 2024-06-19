ee5f12f1fab9e3989d83e868d100e29dd95b90a6
ggrambler <jha02032003@gmail.com>
2024-06-19 14:04:14 +0530

    classesAndObjectinJS

--- artsite/script.js
+++ artsite/script.js
@@ -1,21 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbar');
-    const aniwords = document.getElementById('aniwords');
-    const messages = ["Buy Art Online.", "Art by Archana"];
     let lastScrollTop = 0;
-    let messageIndex = 0;
-
-    // Function to toggle messages
-    function toggleMessage() {
-        messageIndex = (messageIndex + 1) % messages.length;
-        aniwords.textContent = messages[messageIndex];
-    }
-
-    // Change message every 3 seconds
-    setInterval(toggleMessage, 3000);
+    let delta = 5; // Adjust this value to control sensitivity of scroll direction detection
 
     window.addEventListener('scroll', () => {
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (Math.abs(lastScrollTop - scrollTop) <= delta) {
+            return; // If scroll distance is less than delta, ignore
+        }
 
         if (scrollTop > lastScrollTop) {
             // Scroll down
--- js practice/script.js
+++ js practice/script.js
@@ -1,24 +1,15 @@
 
-function frd(...foods){
-    console.log(foods);
-}
-function getf(...f){
-    return f;
-}
-function sum(...a){
-    let res = 0;
-    for(let num of a){
-        res += num;
+
+const person = {
+    //properties
+    firstname:"Divyansh",
+    lastname:"Bhardwaj",
+    age: 22,
+    isEmp: false,
+    sayhello : function(){
+        console.log(`Hi I am ${this.firstname}`);
     }
-    return res;
 }
-const f1 = "pizza";
-const f2 = "wet";
-const f3 = "kidd";
-const f4 = "roll";
 
-frd(f1,f2,f3,f4)
-const foods = getf(f1,f2,f3,f4);
-console.log(foods);
-
-console.log(sum(1,2,3));
+console.log(person.firstname);
+console.log(person.age);
